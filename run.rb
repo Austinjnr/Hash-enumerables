e367e47a70b78bac1cbf614a2b304f294b075d7a
austinjnr <austinobonyo@gmail.com>
2023-02-13 00:26:36 +0300

    practice on hash enumerables

--- run.rb
+++ run.rb
@@ -1,6 +1,6 @@
 require "pry"
 #array
-books = [
+book = 
     {
         title: "Book 1",
         price: 25.65,
@@ -9,4 +9,14 @@ books = [
         rating: "Rating A",
         description: "Description A"
     }
-]
+
+
+#book.each{|b| binding.pry}
+#book.each_key{|b| binding.pry}
+#book.each_value{|b| binding.pry}
+#puts book.keys
+#puts book.values
+author = book.select{|k,v| v==25.65}
+#puts book.include?(:rating)
+puts book.index("Author A")
+binding.pry 
