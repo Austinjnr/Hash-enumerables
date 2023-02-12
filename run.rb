85b0852d9da9d3c9512c0f99e9253d575fbce28d
austinjnr <austinobonyo@gmail.com>
2023-02-13 00:10:05 +0300

    creates array data

---
+++ run.rb
@@ -0,0 +1,12 @@
+require "pry"
+#array
+books = [
+    {
+        title: "Book 1",
+        price: 25.65,
+        author: "Author A",
+        publisher: "Publisher A",
+        rating: "Rating A",
+        description: "Description A"
+    }
+]
